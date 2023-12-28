75f2b1d1b6420656d31b043d253e1f167990c4e9
Divya Raj <divyaraj7612@gmail.com>
2023-12-28 15:48:34 +0530

    cors-Options updated

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,8 +1,10 @@
 
 
 const corsOptions = {
-  origin: 'http://localhost:5173', // Allow requests from this origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  origin: 'http://localhost:5173', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  maxAge: 1000
 };
 module.exports = corsOptions;
