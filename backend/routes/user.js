ba664684a84d332c33798e9544e5b8c915a778a0
Divya Raj <divyaraj7612@gmail.com>
2023-12-28 15:49:12 +0530

    routes updated with middleware functions

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,8 +4,9 @@ const checkToken = require("../middleware/checkToken");
 const verifyJwt = require("../middleware/verifyJwt");
 const router = express.Router();
 
-router.route("/").get(checkToken,verifyJwt)
+router.route('/').get(UserController)
+router.route("/dashboard").get(checkToken,verifyJwt)
 router.route("/register").post(Register)
-router.route("/login").post(Login,checkToken,verifyJwt)
+router.route("/login").post(Login)
 
 module.exports = router;
