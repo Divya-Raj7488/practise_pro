1660195b6664a548f2d23ef8036198412b3a3745
Divya Raj <divyaraj7612@gmail.com>
2024-04-21 01:03:27 +0530

    rectified userRoute

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,11 +9,12 @@ const checkToken = require("../middleware/checkToken");
 const { CreatePost, FetchPosts } = require("../controller/postController");
 const router = express.Router();
 
-router.route("/").get(checkToken, UserController);
-router.route("/profilePic").put(checkToken, UpdateProfilePic);
+
 router.route("/register").post(Register);
 router.route("/login").post(Login);
 router.route("/posts").post(CreatePost);
+router.route("/").get(checkToken, UserController);
+router.route("/profilePic").put(checkToken, UpdateProfilePic);
 router.route("/fetchposts").get(checkToken, FetchPosts);
 
 
